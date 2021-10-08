16209f18831face66292c08ba43f7bb3671bb188
Ivo Pereira <ivopereira15@gmail.com>
2021-10-08 16:45:53 +0100

    fix home cards

--- src/app/modules/add-listing/card/card.component.ts
+++ src/app/modules/add-listing/card/card.component.ts
@@ -8,63 +8,15 @@ import { map, debounceTime, tap, merge, delay, mapTo, share, repeat, switchMap, 
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-  height;
-  width;
-  backgroundImage;
-  mouseX = 0;
-  mouseY = 0;
-  get mousePX() {
-    return this.mouseX / this.width;
-  }
-  get mousePY() {
-    return this.mouseY / this.height;
-  }
+
 
   @Input() cardBgImage: string = '';
   @Input() cardText: string = '';
   @ViewChild('card', { static: true }) card: ElementRef;
-  cardStyling = this.cardStyle();
 
-  cardStyle() {
-    return this.transformStyle();
-  }
-
-  cardBgTransform() {
-
-    return this.transformStyle();
-  }
-
-  private transformStyle() {
-    const tX = this.mousePX * -30;
-    const tY = this.mousePY * -30;
-    return { transform: `rotateY(${tX}deg) rotateX(${tY}deg)` };
-  }
-  get nativeElement(): HTMLElement {
-    return this.card.nativeElement;
-  }
   constructor() { }
 
   ngOnInit(): void {
-    const mouseMove$ = fromEvent<MouseEvent>(this.card.nativeElement, 'mousemove');
-    const mouseLeave$ = fromEvent<MouseEvent>(this.card.nativeElement, 'mouseleave').pipe(
-      delay(100),
-      mapTo(({ mouseX: 0, mouseY: 0 })),
-      share()
-    )
-    const mouseEnter$ = fromEvent<MouseEvent>(this.card.nativeElement, 'mouseenter').pipe(takeUntil(mouseLeave$))
-
-    mouseEnter$.pipe(
-      switchMap(() => mouseMove$),
-      map(e => ({ mouseX: e.pageX - this.nativeElement.offsetLeft - this.width / 2, mouseY: e.pageY - this.nativeElement.offsetTop - this.height / 2 }))
-      , merge(mouseLeave$), repeat()
-    ).subscribe(e => {
-      this.mouseX = e.mouseX;
-      this.mouseY = e.mouseY;
-    })
-
   }
-  ngAfterViewInit() {
-    this.width = this.card.nativeElement.offsetWidth;
-    this.height = this.card.nativeElement.offsetHeight;
-  }
+
 }
